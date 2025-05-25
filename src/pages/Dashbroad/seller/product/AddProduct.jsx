f4099254eb2d114d0be63ead64f8cd1440b5591f
Rajibbepary <rajib635356@gmail.com>
2025-05-25 11:38:04 +0600

    addproduct page from add

--- src/pages/Dashbroad/seller/product/AddProduct.jsx
+++ src/pages/Dashbroad/seller/product/AddProduct.jsx
@@ -1,51 +1,154 @@
 
 
 const AddProduct = () => {
-    return (
-       <div className="py-10 flex flex-col justify-between bg-white">
-            <form className="md:p-10 p-4 space-y-5 max-w-lg">
-                <div>
-                    <p className="text-base font-medium">Product Image</p>
-                    <div className="flex flex-wrap items-center gap-3 mt-2">
-                        {Array(4).fill('').map((_, index) => (
-                            <label key={index} htmlFor={`image${index}`}>
-                                <input accept="image/*" type="file" id={`image${index}`} hidden />
-                                <img className="max-w-24 cursor-pointer" src="https://raw.githubusercontent.com/prebuiltui/prebuiltui/main/assets/e-commerce/uploadArea.png" alt="uploadArea" width={100} height={100} />
-                            </label>
-                        ))}
-                    </div>
-                </div>
-                <div className="flex flex-col gap-1 max-w-md">
-                    <label className="text-base font-medium" htmlFor="product-name">Product Name</label>
-                    <input id="product-name" type="text" placeholder="Type here" className="outline-none md:py-2.5 py-2 px-3 rounded border border-gray-500/40" required />
-                </div>
-                <div className="flex flex-col gap-1 max-w-md">
-                    <label className="text-base font-medium" htmlFor="product-description">Product Description</label>
-                    <textarea id="product-description" rows={4} className="outline-none md:py-2.5 py-2 px-3 rounded border border-gray-500/40 resize-none" placeholder="Type here"></textarea>
-                </div>
-                <div className="w-full flex flex-col gap-1">
-                    <label className="text-base font-medium" htmlFor="category">Category</label>
-                    <select id="category" className="outline-none md:py-2.5 py-2 px-3 rounded border border-gray-500/40">
-                        <option value="">Select Category</option>
-                        {[{ name: 'Electronics' }, { name: 'Clothing' }, { name: 'Accessories' }].map((item, index) => (
-                            <option key={index} value={item.name}>{item.name}</option>
-                        ))}
-                    </select>
-                </div>
-                <div className="flex items-center gap-5 flex-wrap">
-                    <div className="flex-1 flex flex-col gap-1 w-32">
-                        <label className="text-base font-medium" htmlFor="product-price">Product Price</label>
-                        <input id="product-price" type="number" placeholder="0" className="outline-none md:py-2.5 py-2 px-3 rounded border border-gray-500/40" required />
-                    </div>
-                    <div className="flex-1 flex flex-col gap-1 w-32">
-                        <label className="text-base font-medium" htmlFor="offer-price">Offer Price</label>
-                        <input id="offer-price" type="number" placeholder="0" className="outline-none md:py-2.5 py-2 px-3 rounded border border-gray-500/40" required />
-                    </div>
-                </div>
-                <button className="px-8 py-2.5 bg-orange-500 text-white font-medium rounded">ADD</button>
-            </form>
+  
+    const handleSubmit = async(e) =>{
+            e.preventDefault();
+            const from = e.target;
+            const product_image = [];
+                    for (let i = 0; i < 4; i++) {
+                    const input = e.target[`image${i}`];
+                    if (input && input.files.length > 0) {
+                        product_image.push(input.files[0]);
+                    }
+                    }
+            const product_title = from.product_title.value;
+            const description = from.description.value;
+            const category = from.category.value;
+            const price = from.price.value;
+            const quantity = from.quantity.value;
+            const addproduct = {product_image, product_title, description, price, category, quantity,}
+
+            console.log(addproduct)
+    }
+   
+  return (
+    <div className="py-10 flex flex-col justify-between bg-white">
+      <form
+        onSubmit={handleSubmit}
+        className="md:p-10 p-4 space-y-5 max-w-lg"
+      >
+        {/* Product Image */}
+        <div>
+          <p className="text-base font-medium">Product Image</p>
+          <div className="flex flex-wrap items-center gap-3 mt-2">
+            {Array(4)
+              .fill("")
+              .map((_, index) => (
+                <label key={index} htmlFor={`image${index}`}>
+                  <input
+                    required
+                    accept="image/*"
+                    type="file"
+                    name="product_image"
+                    id={`image${index}`}
+                    hidden
+                  />
+                  <img
+                    className="max-w-24 cursor-pointer"
+                    src="https://raw.githubusercontent.com/prebuiltui/prebuiltui/main/assets/e-commerce/uploadArea.png"
+                    alt="uploadArea"
+                    width={100}
+                    height={100}
+                  />
+                </label>
+              ))}
+          </div>
         </div>
-    );
+
+        {/* Product Name */}
+        <div className="flex flex-col gap-1 max-w-md">
+          <label className="text-base font-medium" htmlFor="product-name">
+            Product Name
+          </label>
+          <input
+            required
+            id="product-name"
+            type="text"
+            placeholder="Type here"
+            className="outline-none md:py-2.5 py-2 px-3 rounded border border-gray-500/40"
+           name="product_title"
+          />
+        </div>
+
+        {/* Product Description */}
+        <div className="flex flex-col gap-1 max-w-md">
+          <label
+            className="text-base font-medium"
+            htmlFor="product-description"
+          >
+            Product Description
+          </label>
+          <textarea
+            required
+            id="product-description"
+            rows={4}
+            placeholder="Type here"
+            className="outline-none md:py-2.5 py-2 px-3 rounded border border-gray-500/40 resize-none"
+           name="description"
+          ></textarea>
+        </div>
+
+        {/* Category */}
+        <div className="w-full flex flex-col gap-1">
+          <label className="text-base font-medium" htmlFor="category">
+            Category
+          </label>
+          <select
+            required
+            id="category"
+            className="outline-none md:py-2.5 py-2 px-3 rounded border border-gray-500/40"
+              name="category"
+          >
+            <option value="">Select Category</option>
+            {["Smart Watch", "Mobile", "Laptop"].map((name, index) => (
+              <option key={index} value={name}>
+                {name}
+              </option>
+            ))}
+          </select>
+        </div>
+
+        {/* Price and Quantity */}
+        <div className="flex items-center gap-5 flex-wrap">
+          <div className="flex-1 flex flex-col gap-1 w-32">
+            <label className="text-base font-medium" htmlFor="product-price">
+              Product Price
+            </label>
+            <input
+                required
+              id="product-price"
+              type="number"
+              placeholder="0"
+              className="outline-none md:py-2.5 py-2 px-3 rounded border border-gray-500/40"
+                name="price"
+            />
+          </div>
+          <div className="flex-1 flex flex-col gap-1 w-32">
+            <label className="text-base font-medium" htmlFor="Quantity">
+              Quantity
+            </label>
+            <input
+                required
+              id="Quantity"
+              type="number"
+              placeholder="0"
+              className="outline-none md:py-2.5 py-2 px-3 rounded border border-gray-500/40"
+              name="quantity"
+            />
+          </div>
+        </div>
+
+        {/* Submit Button */}
+        <button
+          type="submit"
+          className="px-8 py-2.5 bg-orange-500 text-white font-medium rounded"
+        >
+          ADD
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default AddProduct;
